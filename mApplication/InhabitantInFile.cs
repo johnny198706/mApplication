70c391f815af5eba5e9735051d39173994f8763d
Dell <Dell@DESKTOP-5DG1PGE>
2024-01-09 14:54:06 +0100

    Dzień 4

--- mApplication/InhabitantInFile.cs
+++ mApplication/InhabitantInFile.cs
@@ -29,64 +29,9 @@
             }
         }
 
-        public override void AddMeasurement(string measurement)
+        public new void AddMeasurement(string measurement)
         {
-            if (float.TryParse(measurement, out float result))
-            {
-                this.AddMeasurement(result);
-            }
-            else if (char.TryParse(measurement, out char charResult))
-            {
-                switch (charResult)
-                {
-                    case 'A':
-                    case 'a':
-                        AddMeasurement(100);
-                        break;
-                    case 'B':
-                    case 'b':
-                        AddMeasurement(200);
-                        break;
-                    case 'C':
-                    case 'c':
-                        AddMeasurement(300);
-                        break;
-                    case 'D':
-                    case 'd':
-                        AddMeasurement(400);
-                        break;
-                    case 'E':
-                    case 'e':
-                        AddMeasurement(500);
-                        break;
-                    case 'F':
-                    case 'f':
-                        AddMeasurement(600);
-                        break;
-                    case 'G':
-                    case 'g':
-                        AddMeasurement(700);
-                        break;
-                    case 'H':
-                    case 'h':
-                        AddMeasurement(800);
-                        break;
-                    case 'I':
-                    case 'i':
-                        AddMeasurement(900);
-                        break;
-                    case 'J':
-                    case 'j':
-                        AddMeasurement(1000);
-                        break;
-                    default:
-                        throw new Exception(" Wrong Letter");
-                }
-            }
-            else
-            {
-                throw new Exception(" string is not float");
-            }
+            base.AddMeasurement(measurement);
         }
 
         public override void AddMeasurement(double measurement)
