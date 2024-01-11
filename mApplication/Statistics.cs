6c7ca7ab9902e2bc0eca3449abc86ea1a6449c3c
Dell <Dell@DESKTOP-5DG1PGE>
2024-01-11 17:11:53 +0100

    Dzień 6

--- mApplication/Statistics.cs
+++ mApplication/Statistics.cs
@@ -3,9 +3,7 @@
     public class Statistics
     {
         public float Min { get; private set; }
-
         public float Max { get; private set; }
-
         public float Sum { get; private set; }
         public int Count { get; private set; }
 
